9dfb30f23b4510f3c460e02fa759f5e4fdba6727
Jefferson Wu <makerjeffwu@gmail.com>
2017-02-02 18:01:22 -0800

    latest and greatest, working opencv video capture app

--- opencv5.py
+++ opencv5.py
@@ -1,17 +1,28 @@
+import argparse
 import cv2
 import numpy as np
 
+#init argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-O", "--output", required=True,
+                help="path to output video. must be unique")
+args = vars(ap.parse_args())
+
+
+#create video capture
 cap = cv2.VideoCapture(1)
 
-#define codec and create videowriteer object
 
+
+#define codec and create videoWriter
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('/output/output.mp4', fourcc, 30.0, (1280,1024))
+out = cv2.VideoWriter('output/' + args["output"], fourcc, 10.0, (1280,1024))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     if ret==True:
-        frame = cv2.flip(frame, 0)
+        # frame = cv2.flip(frame, 1)
 
         #write out flipped frames
         out.write(frame)
